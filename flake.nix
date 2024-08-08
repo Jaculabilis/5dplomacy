7b2176b1d2dfb0c58c585cb704ff2324be28e147
Tim Van Baak <tim.vanbaak@gmail.com>
2024-08-08 07:49:26 -0700

    Update nixpkgs pin

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "5D Diplomacy With Multiversal Time Travel";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/24.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
