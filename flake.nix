26e268c3a08496e31ed615bf782155436593cc7a
Tim Van Baak <tim.vanbaak@gmail.com>
2024-08-08 07:45:27 -0700

    Remove custom PS1

--- flake.nix
+++ flake.nix
@@ -8,9 +8,6 @@
       let pkgs = nixpkgs.legacyPackages.${system};
       in rec {
         devShell = pkgs.mkShell {
-          shellHook = ''
-            PS1='\[\e[0;94m\]\u@\H\[\e[0;93m\] 5dplomacy \[\e[0;92m\]$(git rev-parse --short HEAD)\[\e[0m\]\n\W$ '
-          '';
           DOTNET_CLI_TELEMETRY_OPTOUT = 1;
           packages = [ pkgs.dotnet-sdk pkgs.dotnetPackages.NUnit3 ];
         };
